e90e1dc17a430c3421d00cc729decb68408f4e22
Illia0308 <iluxa.stalker@gmail.com>
2022-01-17 20:49:04 +0100

    54

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,4 @@
-import { getSquaredNumbers, getOddNumbers } from "./calculator";
+import getSum, { getSquaredNumbers, getOddNumbers } from "./calculator";
 
 it("should get squared numbers", () => {
   const result = getSquaredNumbers([1, 2, 3]);
@@ -8,4 +8,9 @@ it("should get squared numbers", () => {
 it("Should keep odd numbers", () => {
   const result = getOddNumbers([1, 2, 3, 4, 5]);
   expect(result).toEqual([1, 3, 5]);
+});
+
+it("get sum of numbers", () => {
+  const result = getSum([1, 5]);
+  expect(result).toEqual(6);
 });
